f16e8dec6ace1314f38cb30756955417bb630bd2
Erwan Soulas <erwan.soulas@gmail.com>
2025-03-10 15:52:51 +0100

    7th commit: fix issue with progressBar ui

--- app/components/ui/progress.tsx
+++ app/components/ui/progress.tsx
@@ -3,29 +3,52 @@ import * as ProgressPrimitive from '@radix-ui/react-progress';
 
 import { cn } from '~/lib/utils';
 
-function Progress(
-    {
-        className,
-        value,
-        ...props
-    }: React.ComponentProps<typeof ProgressPrimitive.Root>
-) {
-    return (
-        <ProgressPrimitive.Root
-            data-slot='progress'
-            className={cn(
-                'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
-                className
-            )}
-            {...props}
-        >
-            <ProgressPrimitive.Indicator
-                data-slot='progress-indicator'
-                className= 'bg-primary h-full w-full flex-1 transition-all'
-                style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-            />
-        </ProgressPrimitive.Root>
-    );
+interface CustomProgressProps
+    extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+    indicatorColor: string;
 }
+
+const Progress = React.forwardRef<
+    React.ComponentRef<typeof ProgressPrimitive.Root>,
+    CustomProgressProps
+>(({ className, value, indicatorColor, ...props }, ref) => (
+    <ProgressPrimitive.Root
+        ref={ref}
+        className={cn(
+            'relative h-4 w-full overflow-hidden rounded-full bg-stone-900/20 dark:bg-neutral-50/20',
+            className
+        )}
+        {...props}
+    >
+        <ProgressPrimitive.Indicator
+            className={`h-full w-full flex-1 transition-all dark:bg-neutral-50 ${indicatorColor}`}
+            style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        />
+    </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+// function Progress({
+//     className,
+//     value,
+//     ...props
+// }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+//     return (
+//         <ProgressPrimitive.Root
+//             data-slot='progress'
+//             className={cn(
+//                 'bg-stone-900/20 relative h-4 w-full overflow-hidden rounded-full',
+//                 className
+//             )}
+//             {...props}
+//         >
+//             <ProgressPrimitive.Indicator
+//                 data-slot='progress-indicator'
+//                 className=' h-full w-full flex-1 transition-all'
+//                 style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+//             />
+//         </ProgressPrimitive.Root>
+//     );
+// }
 
 export { Progress };
