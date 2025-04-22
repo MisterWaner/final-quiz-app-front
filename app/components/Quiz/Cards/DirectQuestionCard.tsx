0af1d846c5caf430ec9e5e1ec3533900d8013bcd
Erwan Soulas <erwan.soulas@gmail.com>
2025-04-22 21:26:21 +0200

    7th commit: --start gaming ui

---
+++ app/components/Quiz/Cards/DirectQuestionCard.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    CardTitle,
+    CardFooter
+} from '~/components/ui/card';
+import { Label } from '~/components/ui/label';
+import { Input } from '~/components/ui/input';
+import Timer from '../Timer';
+import { useQuizStore } from '~/store/quiz-store';
+
+
+export default function DirectQuestionCard() {
+  return (
+    <div>DirectQuestionCard</div>
+  )
+}
---
+++ app/routes/quizRoute.tsx
@@ -0,0 +1,5 @@
+import QuizPage from "~/pages/QuizPage";
+
+export default function quizRoute() {
+    return <QuizPage />;
+}
