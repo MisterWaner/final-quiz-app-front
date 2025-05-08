da8960865105e294f66c5134fd5ccf605f62a14a
Erwan Soulas <erwan.soulas@gmail.com>
2025-05-08 14:23:52 +0200

    16th commit: --add auth logic functions

--- app/services/auth-service.ts
+++ app/services/auth-service.ts
@@ -1,10 +1,10 @@
 import type { User } from '~/lib/types';
 
-const BASE_URL = 'http://localhost:3001/api/users'
+const BASE_URL = 'http://localhost:3001/api/auth'
 
 export async function registerUser(user: User) {
     try {
-        const response = await fetch(BASE_URL, {
+        const response = await fetch(`${BASE_URL}/register`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -27,6 +27,81 @@ export async function registerUser(user: User) {
     }
 }
 
-export async function loginUser(user: User) {}
+export async function loginUser(user: User) {
+    try {
+        const response = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(user),
+        });
 
-export async function logoutUser(user: User) {}
+        if (!response.ok) {
+            throw new Error("Erreur lors de l'authentification");
+        }
+
+        const responseData = await response.json();
+        console.log(responseData, 'Utilisateur authentifié');
+        return responseData;
+    } catch (error) {
+        console.error(
+            "Une erreur est survenue lors de l'authentification",
+            error
+        );
+        throw error;
+    }
+}
+
+export async function checkUser() {
+    try {
+        const response = await fetch(`${BASE_URL}/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        })
+
+        if (!response.ok) {
+            throw new Error("Vous n'êtes pas autorisé à accéder à cette page");
+        }
+        const responseData = await response.json();
+        return responseData;
+
+    } catch (error) {
+        console.error(
+            "Une erreur est survenue lors de la vérification de l'authentification",
+            error
+        );
+        throw error;
+    }
+}
+
+export async function logoutUser(user: User) {
+    try {
+        const response = await fetch(`${BASE_URL}/logout`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(user),
+        });
+
+        if (!response.ok) {
+            throw new Error("Erreur lors de la déconnexion");
+        }
+
+        const responseData = await response.json();
+        console.log(responseData, 'Utilisateur déconnecté');
+        return responseData;
+    } catch (error) {
+        console.error(
+            "Une erreur est survenue lors de la déconnexion",
+            error
+        );
+        throw error;
+    }
+}
